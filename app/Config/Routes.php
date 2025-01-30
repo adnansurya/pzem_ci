fbc435740f1b7dd922ec9b0848558198ce79d37e
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-01-31 00:59:47 +0800

    fix history cant open

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'SensorController::index');
-$routes->get('/history', 'SensorController::history');
+// $routes->get('/history', 'SensorController::history');
 $routes->post('/save-data', 'SensorController::saveData');
 $routes->get('/dashboard', 'Dashboard::index');
 $routes->get('/history', 'History::index');
