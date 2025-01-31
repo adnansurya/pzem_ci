a2bd47d09092cfe82db4d4ff55868b683169fce4
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-01-31 12:21:27 +0800

    Update history.php

--- app/Views/history.php
+++ app/Views/history.php
@@ -11,29 +11,35 @@
             <th>Tegangan (V)</th>
             <th>Frekuensi (Hz)</th>
             <th>Daya (W)</th>
+            <th>Pf</th>
+            <th>Energy (Wh)</th>
             <th>Status</th>
             <th>Waktu</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($sensorHistory as $sensor): ?>
-        <tr>
-            <td><?= esc($sensor['phase']) ?></td>
-            <td><?= esc($sensor['current']) ?></td>
-            <td><?= esc($sensor['voltage']) ?></td>
-            <td><?= esc($sensor['frequency']) ?></td>
-            <td><?= esc($sensor['power']) ?></td>
-            <td>
-                <?php if ($sensor['status'] == 'Critical'): ?>
-                    <span class="badge bg-danger">Critical</span>
-                <?php elseif ($sensor['status'] == 'Warning'): ?>
-                    <span class="badge bg-warning text-dark">Warning</span>
-                <?php else: ?>
-                    <span class="badge bg-success">Normal</span>
-                <?php endif; ?>
-            </td>
-            <td><?= esc($sensor['created_at']) ?></td>
-        </tr>
+            <tr>
+                <td><?= esc($sensor['phase']) ?></td>
+                <td><?= esc($sensor['current']) ?></td>
+                <td><?= esc($sensor['voltage']) ?></td>
+                <td><?= esc($sensor['frequency']) ?></td>
+                <td><?= esc($sensor['power']) ?></td>
+                <td><?= esc($sensor['pf']) ?></td>
+                <td><?= esc($sensor['energy']) ?></td>
+                <td>
+                    <?php if ($sensor['status'] == 'On'): ?>
+                        <span class="badge bg-success">ON</span>
+                    <?php elseif ($sensor['status'] == 'Off'): ?>
+                        <span class="badge bg-danger">OFF</span>
+                    <?php elseif ($sensor['status'] == 'Disconnected'): ?>
+                        <span class="badge bg-default text-dark">Disconnected</span>
+                    <?php else: ?>
+                        <span class="badge bg-warning text-datk"><?= esc($sensor['power']) ?></span>
+                    <?php endif; ?>
+                </td>
+                <td><?= esc($sensor['created_at']) ?></td>
+            </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
