542a1cd43292cafa7e85c0a933a8a4d15dc83b17
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-02-08 06:03:19 +0800

    fix bug url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://pzem.local/';
+    public string $baseURL = 'http://192.168.8.14/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
