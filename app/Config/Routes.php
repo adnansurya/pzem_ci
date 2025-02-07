ce2334927cd8732f2b068d9d62aef0fbd623f2a6
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-02-08 05:18:01 +0800

    sistem login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,21 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'SensorController::index');
+// $routes->get('/', 'SensorController::index');
 // $routes->get('/history', 'SensorController::history');
 $routes->post('/save-data', 'SensorController::saveData');
-$routes->get('/dashboard', 'Dashboard::index');
+// $routes->get('/dashboard', 'Dashboard::index');
 $routes->get('/get-latest-data', 'Dashboard::getLatestData');
-$routes->get('/history', 'History::index');
+// $routes->get('/history', 'History::index');
+
+$routes->get('/login', 'Auth::login');
+$routes->post('/login', 'Auth::doLogin');
+$routes->get('/logout', 'Auth::logout');
+
+$routes->group('', ['filter' => 'auth'], function($routes) {
+    $routes->get('/', 'Dashboard::index');
+    $routes->get('/dashboard', 'Dashboard::index');
+    $routes->get('/history', 'History::index');
+});
+
 
